3c8487f9057a0454a96df93ce0d9d4b709e5175f
runner76 <chubchubgiri@outlook.com>
2023-09-25 04:50:36 -0600

    feat: export function is added
    Export function is added, rendering crude version of the program ready to use

--- bin/img2text.py
+++ bin/img2text.py
@@ -7,31 +7,33 @@ import os
 
 main_dir = os.getcwd()
 image_path = main_dir + "/img"
+text_path = main_dir + "/text"
 
 class Img2text(object):
 
-  def __init__(self, image):
-    image_path = self.imagePath(image)
+  def __init__(self, filename):
+    self.filename = filename
+    image_path = self.imagePath(filename)
     print(image_path)
     self.image = Image.open(image_path)
-    print(self.image)
+    self.text = self.conversion(self.image) 
 
-  def imagePath(self, image):
-    file = glob(image_path + f'/*{image}*.*')
-    return file
+  def imagePath(self, filename):
+    for file in glob(image_path + f'/*{filename}*.*'):
+      return file
 
-  def conversion(self):
-
-    # Providing the tesseract executable
-    # location to pytesseract library
-    #pytesseract.tesseract_cmd = path_to_tesseract
-      
-    # Passing the image object to image_to_string() function
-    # This function will extract the text from the image
-    text = pytesseract.image_to_string(self.image)
-      
-    # save the extracted text
+  def conversion(self, image):
+    text = pytesseract.image_to_string(image)
     return text[:-1]
+  
+  def export(self, text):
+    txt_file = open(text_path + f"/{self.filename}.txt", 'w')
+    txt_file.write(text)
+    txt_file.close()
+    return txt_file
+   
+  def run(self):
+    self.export(self.text)
 
 def main():
   # prompt user to enter filename
@@ -39,6 +41,6 @@ def main():
 
   # run program
   program = Img2text(filename)
-  print(program.conversion())
+  program.run()
 
 main()
